63e2f7271eb38bac4f7018642c01585d1fd836b6
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-01-08 13:57:36 +0200

    hashtable: Add string test

--- hashtable.c
+++ hashtable.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <regex.h>
 
 struct hashobj {
 	struct hashobj *next;
@@ -32,15 +33,17 @@ static int hashtable_init(struct hashtable *tbl, size_t size,
 	return 0;
 }
 
-static int hashtable_put(struct hashtable *tbl, const void *key, void *data)
+static int hashtable_set(struct hashtable *tbl, const void *key, void *data)
 {
 	size_t i = tbl->hash(key) % tbl->nbuckets;
 	struct hashobj *o = tbl->buckets[i];
 	struct hashobj *prev = NULL;
 	while (o) {
 		prev = o;
-		if (!tbl->cmp(o->key, key))
-			return -EEXIST;
+		if (!tbl->cmp(o->key, key)) {
+			o->data = data;
+			return 0;
+		}
 		o = o->next;
 	}
 
@@ -119,7 +122,7 @@ static int numcmp(const void *key1, const void *key2)
 	return !(key1 == key2);
 }
 
-int main()
+void numtest()
 {
 	int N = 512;
 	int nbuckets = 256;
@@ -128,20 +131,17 @@ int main()
 	struct hashtable tbl = { 0 };
 	int r = hashtable_init(&tbl, nbuckets, numhash, numcmp);
 	if (r < 0) {
-		perror("hashtable_init");
+		fprintf(stderr, "hashtable_init: %s\n", strerror(-r));
 		exit(1);
 	}
+	printf("random number test with %d buckets and %d numbers\n", nbuckets, N);
 	srandom(time(NULL));
 	for (int i = 0; i < N; ++i) {
 		long num = random();
-		r = hashtable_put(&tbl, (void *)num, (void *)1);
+		r = hashtable_set(&tbl, (void *)num, (void *)1);
 		if (r < 0) {
-			if (r == -EEXIST)
-				printf("%ld already exists\n", num);
-			else {
-				perror("hashtable_put");
-				exit(1);
-			}
+			fprintf(stderr, "hashtable_set: %s\n", strerror(-r));
+			exit(1);
 		}
 	}
 	for (int i = 0; i < tbl.nbuckets; ++i) {
@@ -155,5 +155,86 @@ int main()
 	}
 	printf("avg bucket (aka load factor) %.2f\n", avg);
 	printf("avg bucket deviation %.2f\n", dev / nbuckets);
+}
+
+void stringtest()
+{
+	struct hashtable tbl = { 0 };
+	int r = hashtable_init(&tbl, 256, strhash, _strcmp);
+	if (r < 0) {
+		fprintf(stderr, "hashtable_init: %s\n", strerror(-r));
+		exit(1);
+	}
+
+	regex_t regex;
+	r = regcomp(&regex, "[[:alpha:]][[:alnum:]_-]+", REG_EXTENDED);
+	if (r) {
+		fprintf(stderr, "error compiling regex\n");
+		exit(1);
+	}
+
+	int N = 0;
+	char buf[4096];
+	for (;;) {
+		size_t n = fread(buf, 1, sizeof(buf) - 1, stdin);
+		if (!n) {
+			if (feof(stdin))
+				break;
+			perror("fread");
+			exit(1);
+		}
+		buf[n] = '\0';
+
+		char *s = buf;
+		regmatch_t match;
+		while (s < &buf[n] && !regexec(&regex, s, 1, &match, 0)) {
+			size_t cnt = 1;
+			size_t len = match.rm_eo - match.rm_so;
+			char *key = s + match.rm_so;
+			key[len] = '\0';
+			void *data = hashtable_get(&tbl, key);
+			if (data)
+				cnt = (size_t)data + 1;
+			else {
+				++N;
+				void *k = malloc(len + 1);
+				if (!k) {
+					perror("malloc");
+					exit(1);
+				}
+				memcpy(k, key, len + 1);
+				key = k;
+			}
+			r = hashtable_set(&tbl, key, (void *)cnt);
+			if (r < 0) {
+				fprintf(stderr, "hashtable_set: %s\n", strerror(-r));
+				exit(1);
+			}
+			s += match.rm_eo + 1;
+		}
+	}
+
+	float avg = (float)N / tbl.nbuckets;
+	float dev = 0;
+	for (int i = 0; i < tbl.nbuckets; ++i) {
+		int n = 0;
+		struct hashobj *o = tbl.buckets[i];
+		while (o) {
+			++n;
+			/* printf("%d: %s %zu\n", i, (char *)o->key, (size_t)o->data); */
+			o = o->next;
+		}
+		/* printf("bucket %d: %d keys\n", i, n); */
+		dev += fabsf(avg - n);
+	}
+	printf("string test with %zu buckets and %d strings\n", tbl.nbuckets, N);
+	printf("avg bucket (aka load factor) %.2f\n", avg);
+	printf("avg bucket deviation %.2f\n", dev / tbl.nbuckets);
+}
+
+int main()
+{
+	numtest();
+	stringtest();
 	return 0;
 }
