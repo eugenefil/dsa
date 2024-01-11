f3a3c05bf1ddf5b4b8b9549cb77bc01a372fe7ec
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-01-12 01:28:53 +0200

    rbtree: Replace NULL nodes with nil sentinel

--- rbtree.c
+++ rbtree.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <string.h>
 
 struct rb_node {
 	struct rb_node *parent;
@@ -14,55 +15,81 @@ struct rb_node {
 	int red;
 };
 
-struct rb_root {
-	struct rb_node *node;
+struct rb_tree {
+	struct rb_node *root;
+	/* Sentinel node is used in place of NULL. It's always black.
+	 * It's not strictly needed for inserting, but deleting may set
+	 * its parent - used in the first iteration of the follow-up fixup.
+	 */
+	struct rb_node nil;
 };
 
-static void rb_rotate_left(struct rb_root *root, struct rb_node *node)
+static void rb_init(struct rb_tree *tree)
 {
-	struct rb_node *c = node->right;
-	node->right = c->left;
-	if (c->left)
-		c->left->parent = node;
-
-	if (!node->parent)
-		root->node = c;
-	else if (node == node->parent->left)
-		node->parent->left = c;
-	else
-		node->parent->right = c;
-	c->parent = node->parent;
-
-	c->left = node;
-	node->parent = c;
+	memset(tree, 0, sizeof(*tree));
+	tree->root = &tree->nil;
 }
 
-static void rb_rotate_right(struct rb_root *root, struct rb_node *node)
+#define RB_NIL(n) (!(n)->left)
+
+/*
+*   N             R
+*  / \           / \
+* 1   R   -->   N   3
+*    / \       / \
+*   2   3     1   2
+*/
+static void rb_rotate_left(struct rb_tree *tree, struct rb_node *node)
 {
-	struct rb_node *c = node->left;
-	node->left = c->right;
-	if (c->right)
-		c->right->parent = node;
+	struct rb_node *r = node->right;
+	node->right = r->left;
+	r->left->parent = node;
 
-	if (!node->parent)
-		root->node = c;
+	if (node == tree->root)
+		tree->root = r;
 	else if (node == node->parent->left)
-		node->parent->left = c;
+		node->parent->left = r;
 	else
-		node->parent->right = c;
-	c->parent = node->parent;
+		node->parent->right = r;
+	r->parent = node->parent;
 
-	c->right = node;
-	node->parent = c;
+	r->left = node;
+	node->parent = r;
 }
 
-static void rb_insert(struct rb_root *root, struct rb_node *node,
+/*
+*     N         L
+*    / \       / \
+*   L   1 --> 2   N
+*  / \           / \
+* 2   3         3   1
+*/
+static void rb_rotate_right(struct rb_tree *tree, struct rb_node *node)
+{
+	struct rb_node *l = node->left;
+	node->left = l->right;
+	l->right->parent = node;
+
+	if (node == tree->root)
+		tree->root = l;
+	else if (node == node->parent->left)
+		node->parent->left = l;
+	else
+		node->parent->right = l;
+	l->parent = node->parent;
+
+	l->right = node;
+	node->parent = l;
+}
+
+static void rb_insert(struct rb_tree *tree, struct rb_node *node,
 		      int (*cmp)(const struct rb_node *new, const struct rb_node *old))
 {
-	struct rb_node *parent = NULL;
-	struct rb_node *n = root->node;
+	struct rb_node *nil = &tree->nil;
+	struct rb_node *parent = nil;
+	struct rb_node *n = tree->root;
 	int ret;
-	while (n) {
+	while (n != nil) {
 		parent = n;
 		ret = cmp(node, n);
 		if (ret < 0)
@@ -70,46 +97,64 @@ static void rb_insert(struct rb_root *root, struct rb_node *node,
 		else
 			n = n->right;
 	}
-	node->parent = parent;
-	if (!parent)
-		root->node = node;
+	if (parent == nil)
+		tree->root = node;
 	else if (ret < 0)
 		parent->left = node;
 	else
 		parent->right = node;
-
+	node->parent = parent;
+	node->left = nil;
+	node->right = nil;
 	node->red = 1;
-	while (node->parent && node->parent->red) {
-		/* Here node can't be root, b/c root has no parent.
-		 * Also can't be root's child, b/c root is black.
+
+	while (node->parent->red) {
+		/* Here node can't be root, b/c root's parent, nil, is black.
+		 * Also can't be root's child, b/c root is always black.
 		 * So we're at least root's grandchild.
 		 */
 		struct rb_node *parent = node->parent;
 		struct rb_node *grand = parent->parent;
 		if (parent == grand->left) {
 			struct rb_node *uncle = grand->right;
-			if (uncle && uncle->red) {
+			if (uncle->red) {
 				/* Both parent and uncle are red. This
 				 * means grandparent is black. Swap colors:
 				 * make siblings black and grandparent red.
 				 * Continue tree checking with grandparent.
+				 *
+				 * (uppercase is black, [] denote current node)
+				 *      G          [g]
+				 *     / \         / \
+				 *    p   u -->   P   U
+				 *   /           /
+				 * [n]          n
 				 */
 				parent->red = 0;
 				uncle->red = 0;
 				grand->red = 1;
 				node = grand;
 			} else {
-				/* If node is to the right of parent, rotate
+				/* Parent is red, but uncle is black.
+				 * If node is to the right of parent, rotate
 				 * parent left to become node's left child.
 				 * I.e. move red-red conflict to the left.
 				 * If kept as is (on the right), the conflict
 				 * would remain after rotating grandparent
 				 * right below: red node will be red
 				 * grandparent's left child. Make parent
-				 * (i.e. lower node) new current node.
+				 * (i.e. new lower node) new current node.
+				 *
+				 *     G             G
+				 *    / \           / \
+				 *   p   U -->     n   U
+				 *  / \           /
+				 * *  [n]       [p]
+				 *              /
+				 *             *
 				 */
 				if (node == parent->right) {
-					rb_rotate_left(root, parent);
+					rb_rotate_left(tree, parent);
 					node = parent;
 				}
 				/* Here node is to the left of parent,
@@ -119,42 +164,49 @@ static void rb_insert(struct rb_root *root, struct rb_node *node,
 				 * grandparent right to remove one tree level:
 				 * red grandparent becomes right child of
 				 * black parent. Loop ends after that.
+				 *
+				 *      G          P
+				 *     / \        / \
+				 *    p   U --> [n]  g
+				 *   / \            / \
+				 * [n]  *          *   U
 				 */
+
 				/* node could have been changed above */
 				node->parent->red = 0;
 				grand->red = 1;
-				rb_rotate_right(root, grand);
+				rb_rotate_right(tree, grand);
 			}
 		} else {
 			/* Same as above, but with left and right exchanged. */
 			struct rb_node *uncle = grand->left;
-			if (uncle && uncle->red) {
+			if (uncle->red) {
 				parent->red = 0;
 				uncle->red = 0;
 				grand->red = 1;
 				node = grand;
 			} else {
 				if (node == parent->left) {
-					rb_rotate_right(root, parent);
+					rb_rotate_right(tree, parent);
 					node = parent;
 				}
 				node->parent->red = 0;
 				grand->red = 1;
-				rb_rotate_left(root, grand);
+				rb_rotate_left(tree, grand);
 			}
 		}
 	}
-	root->node->red = 0;
+	tree->root->red = 0;
 }
 
 static void rb_preorder(struct rb_node *node, void (*fn)(struct rb_node *))
 {
-	if (!node)
+	if (RB_NIL(node))
 		return;
 	fn(node);
-	if (node->left)
+	if (!RB_NIL(node->left))
 		rb_preorder(node->left, fn);
-	if (node->right)
+	if (!RB_NIL(node->right))
 		rb_preorder(node->right, fn);
 }
 
@@ -169,18 +221,18 @@ static void gen_dot(struct rb_node *node)
 	unsigned long long nid = (unsigned long long)node;
 	printf("N%llx [label=%ld color=%s]\n", nid, num, node->red ? "red" : "black");
 
-	if (node->left || node->right) {
-		unsigned long long lid = node->left ?
+	if (!RB_NIL(node->left) || !RB_NIL(node->right)) {
+		unsigned long long lid = !RB_NIL(node->left) ?
 			(unsigned long long)node->left :
 			(unsigned long long)&node->left;
-		unsigned long long rid = node->right ?
+		unsigned long long rid = !RB_NIL(node->right) ?
 			(unsigned long long)node->right :
 			(unsigned long long)&node->right;
 		printf("N%llx -- N%llx\n", nid, lid);
 		printf("N%llx -- N%llx\n", nid, rid);
-		if (!node->left)
+		if (RB_NIL(node->left))
 			printf("N%llx [shape=point]\n", lid);
-		if (!node->right)
+		if (RB_NIL(node->right))
 			printf("N%llx [shape=point]\n", rid);
 	}
 }
@@ -267,7 +319,9 @@ int intcmp(const struct rb_node *new, const struct rb_node *old)
 
 int main(int argc, char *argv[])
 {
-	struct rb_root root = { 0 };
+	struct rb_tree tree;
+	rb_init(&tree);
+
 	char *line = NULL;
 	size_t n = 0;
 	for (;;) {
@@ -297,8 +351,8 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 		num->val = val;
-		rb_insert(&root, &num->node, intcmp);
-		print_dot(root.node, &argv[1], false);
+		rb_insert(&tree, &num->node, intcmp);
+		print_dot(tree.root, &argv[1], false);
 	}
 	free(line);
 	return 0;
